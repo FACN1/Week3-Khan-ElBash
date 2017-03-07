b3c0cf82d8bc548d33a6ae76e79dc6f2328f1024
Mario <mario.saliba98@gmail.com>
2017-03-07 15:25:25 +0200

    adds the skeleton for render file

---
+++ render.js
@@ -0,0 +1,16 @@
+var render = (function() {
+
+  var MovieInfo = function(response) {
+
+  }
+
+  var MovieGIF = function(response) {
+
+  }
+
+  return {
+    MovieInfo: MovieInfo,
+    MovieGIF: MovieGIF
+  }
+
+})();
