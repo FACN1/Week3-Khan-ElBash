be0754e0ed9d142dc524c38d744faae39d0be651
Mario <mario.saliba98@gmail.com>
2017-03-07 16:26:31 +0200

    adds request function

--- request.js
+++ request.js
@@ -1,7 +1,14 @@
 var Request = (function () {
 
   var makeRequest = function(method, url, callback) {
-    // need to implement this 
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200){
+        callback(JSON.parse(xhr.responseText));
+      }
+    }
+    xhr.open(method, url);
+    xhr.send();
   }
 
   return {
