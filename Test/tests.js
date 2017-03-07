1fe0955f42e57e83266e3e8b3bcdac7aa8ca04a6
Reem <reemq86@hotmail.com>
2017-03-07 16:34:20 +0200

    initial test modules

---
+++ Test/tests.js
@@ -0,0 +1,30 @@
+QUnit.module( "renderMovieInfo" );
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
+QUnit.test( "a basic test example", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
+QUnit.test( "a basic test example 2", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
+
+/*========================================================================================================*/
+
+QUnit.module( "renderMovieGIF function tests" );
+QUnit.test( "a basic test example 3", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
+QUnit.test( "a basic test example 4", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
+
+/*========================================================================================================*/
+
+QUnit.module( "makeRequest function tests" );
+QUnit.test( "a basic test example 3", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
+QUnit.test( "a basic test example 4", function( assert ) {
+  assert.ok( true, "this test is fine" );
+});
