2ec0c0265b84dd68e21660ad75e22cac06d0cbe6
mohamed omarii <mohamed.44301@hotmail.com>
2017-03-08 17:30:03 +0200

    change click to submit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.querySelector('button').addEventListener('click', function(event){
+document.querySelector('button').addEventListener('submit', function(event){
   event.preventDefault();
   var searchQuery  = document.querySelector('input').value;
   Request.makeRequest(
