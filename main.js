83969229eea1319eaa1d42cc1c93287774765633
mohamed omarii <mohamed.44301@hotmail.com>
2017-03-08 17:32:23 +0200

    change button to form

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.querySelector('button').addEventListener('submit', function(event){
+document.querySelector('form').addEventListener('submit', function(event){
   event.preventDefault();
   var searchQuery  = document.querySelector('input').value;
   Request.makeRequest(
