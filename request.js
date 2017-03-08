1587c36f7ff6cb4c1d10b2cefcafd76e28b71f20
Mario <mario.saliba98@gmail.com>
2017-03-08 10:59:46 +0200

    fix for the pull request for request.js file

--- request.js
+++ request.js
@@ -1,14 +1,6 @@
 var Request = (function () {
 
   var makeRequest = function(method, url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200){
-        callback(JSON.parse(xhr.responseText));
-      }
-    }
-    xhr.open(method, url);
-    xhr.send();
 
     // need to implement this
   }
