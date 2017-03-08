38019b80c8beed933b9e4a6011a970bdf9cff405
Matt Lubel <matt_lubel@hotmail.com>
2017-03-08 18:01:43 +0200

    added to main.js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 document.querySelector('form').addEventListener('submit', function(event){
   event.preventDefault();
   var searchQuery  = document.querySelector('input').value;
+
   Request.makeRequest(
     'GET',
-    'http://www.omdbapi.com/?t='+searchQuery,
+    'http://www.omdbapi.com/?t=' + searchQuery,
      Render.renderMovieInfo
+  );
+
+  Request.makeRequest(
+    'GET',
+    'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=test' + searchQuery,
+    Render.renderMovieGIF
   );
 
 });
