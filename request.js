51b19765df8abb68f4657b43f9636d1959192344
Mario <mario.saliba98@gmail.com>
2017-03-08 10:49:37 +0200

    adds errors to the request func

--- request.js
+++ request.js
@@ -4,13 +4,20 @@ var Request = (function () {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200){
-        callback(JSON.parse(xhr.responseText));
+        callback(null, JSON.parse(xhr.responseText));
+      }
+      else if (xhr.status > 399 && xhr.status < 500) {
+        var errorMessage = "Client Error! Status: " + xhr.status;
+        callback(errorMessage, null);
+      }
+      else if (xhr.status > 499 && xhr.status < 600){
+        var errorMessage = "Server Error! Status: " + xhr.status;
+        callback(errorMessage, null);
       }
     }
     xhr.open(method, url);
     xhr.send();
 
-    // need to implement this
   }
 
   return {
