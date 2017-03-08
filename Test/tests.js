d4ad22e4b2462be78ee9eafbb4286b8327719916
Matt Lubel <matt_lubel@hotmail.com>
2017-03-08 12:31:27 +0200

    add prepareGIFUrls function

--- Test/tests.js
+++ Test/tests.js
@@ -5,10 +5,35 @@ QUnit.test( "hello test", function( assert ) {
 
 /*========================================================================================================*/
 
-QUnit.module( "renderMovieGIF function tests" );
-QUnit.test( "a basic test example 1", function( assert ) {
-  assert.ok( true, "this test is fine" );
+QUnit.module( "prepareGIFUrls function tests" );
+
+QUnit.test("test the function converts the response to an array of length 3", function (assert) {
+  var response = Fixtures.mockGifResponse;
+  var result = Render.prepareGIFUrls(null, response);
+  assert.equal(result.length, 3, "output array should have length 3");
+})
+
+QUnit.test( "test the function returns an array of the right urls", function( assert ) {
+  var response = Fixtures.mockGifResponse;
+  var result = Render.prepareGIFUrls(null, response);
+  var expectedOutput = [
+    "http://media3.giphy.com/media/ZnOmhZv0rRbmU/100w.gif",
+    "http://media2.giphy.com/media/AX1BnSZfXVsu4/100w.gif",
+    "http://media2.giphy.com/media/pWDH6fkHgGHza/100w.gif"
+  ];
+  assert.deepEqual(result, expectedOutput, "output array should have the right urls");
 });
+
+QUnit.test("test the function logs the error if passed an error", function (assert) {
+  var result = Render.prepareGIFUrls(404, null);
+  assert.equal(result, undefined, "function should not return anything if receives an error");
+});
+
+QUnit.test("test the function returns a list of the right number of urls if the response has fewer than 3", function (assert) {
+  var response = Fixtures.mockGifResponseWithOneResult;
+  var result = Render.prepareGIFUrls(null, response);
+  assert.equal(result.length, 1, "output array should have length 1");
+})
 
 /*========================================================================================================*/
 
--- render.js
+++ render.js
@@ -1,15 +1,33 @@
 var Render = (function() {
 
-  var renderMovieInfo = function(response) {
+  var renderMovieInfo = function(error, result) {
 
   };
 
-  var renderMovieGIF = function(response) {
+  var prepareGIFUrls = function (error, result) {
+    // result is in the format of e.g. http://api.giphy.com/v1/gifs/search?q=titanic&api_key=dc6zaTOxFJmzC
+    if (error) {
+      console.log(error);
+      return
+    }
+    var gifUrls = [];
+
+    // numberOfUrls is 3 or number of results (if less than 3)
+    var numberOfUrls = Math.min(result.data.length, 3);
+
+    for (var i = 0; i < numberOfUrls; i++) {
+      gifUrls[i] = result.data[i].images.fixed_width_small.url;
+    }
+    return gifUrls;
+  };
+
+  var renderMovieGIF = function(gifUrls) {
 
   };
 
   return {
     renderMovieInfo: renderMovieInfo,
+    prepareGIFUrls: prepareGIFUrls,
     renderMovieGIF: renderMovieGIF
   }
 
